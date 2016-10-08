e747d9f88bd39e13d358993484a71a0f9a3b3b82
hermitmaster <dmrausch@icloud.com>
2016-10-08 14:21:29 -0500

    Fixed test failure caused by missing db.

--- src/test/java/org/cougars/BookmarkManagerApplicationTests.groovy
+++ src/test/java/org/cougars/BookmarkManagerApplicationTests.groovy
@@ -27,8 +27,10 @@ package org.cougars
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookmarkManagerApplicationTests {
