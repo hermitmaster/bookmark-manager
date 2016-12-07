0bf4c128ca2e465268c79548904b9f1aa59db5ec
hermitmaster <dmrausch@icloud.com>
2016-12-06 21:45:33 -0600

    Fixed js issue causing errors when changing page size after search and when loading an unsorted table.
    Author:    hermitmaster <hermitmaster@icloud.com>

--- src/main/resources/static/js/global.jquery.js
+++ src/main/resources/static/js/global.jquery.js
@@ -7,5 +7,38 @@ jQuery.fn.extend({
         $.ajax({
             url: url
         });
+    },
+
+    changePageSize: function () {
+        var pageSize = $('#page-size').val();
+        var location = window.location.href;
+
+        if(location.indexOf('size=') !== -1) {
+            location = window.location.href.replace(/(page=)(\d+)/g,'page=0');
+            location = location.replace(/(size=)(\d+)/g,'size=' + pageSize);
+        } else {
+            location += (location.indexOf('?') !== -1) ? '&' : '?';
+            location += 'page=0&size=' + pageSize;
+        }
+
+        window.location = location;
+
+        return false;
+    },
+
+    updatePageSize: function () {
+        var urlParams = {};
+        var match;
+        var search = /([^&=]+)=?([^&]*)/g;
+        var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+        var query  = window.location.search.substring(1);
+
+        while (match = search.exec(query)) {
+            urlParams[decode(match[1])] = decode(match[2]);
+        }
+
+        var pageSize = urlParams['size'] != null ? urlParams['size'] : 25;
+
+        $("#page-size").val(pageSize);
     }
 });
